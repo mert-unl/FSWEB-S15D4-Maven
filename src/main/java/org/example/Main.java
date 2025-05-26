22a12cb433dad49e81ba8c897b0a3bda6aa9aa21
troemanes <mertunal1998@gmail.com>
2025-05-26 15:59:45 +0300

    finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,58 @@
 package org.example;
 
 
+import java.util.LinkedList;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+checkForPalindrome("I did, did I?");
+        convertDecimalToBinary(13);
+    }
+
+
+    public static boolean checkForPalindrome(String input) {
+
+        String newInput = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase(Locale.ENGLISH);
+
+        String reverse = new StringBuilder(newInput).reverse().toString();
+
+
+
+        if (newInput.equals(reverse)) {
+            return true;
+        } else return false;
+
+
+    }
+
+
+    public static String convertDecimalToBinary(int input) {
+
+        // % KALANI VERİYOR
+        // / KAÇ KEZ BÖLÜNDÜĞÜNÜ VERİYOR
+
+        int sayi = input;
+        LinkedList a = new LinkedList();
+
+        while (sayi > 1) {
+            int kalan1 = sayi % 2;
+
+            a.addFirst(kalan1);
+
+            sayi = sayi / 2;
+        }
+        if (sayi < 2) {
+            a.addFirst(sayi);
+        }
+        String b = a.toString().replaceAll("[^a-zA-Z0-9]", "");
+        System.out.println(b);
+
+        return b;
+
+
     }
 
 }
